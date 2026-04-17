f92608c89afcf3915ab3783e8eaf1378b169a52b
hisuifeng <minecraftsuifeng@qq.com>
2026-04-18 00:30:48 +0800

    test

--- js/index.js
+++ js/index.js
@@ -5,10 +5,13 @@ var page = document.getElementById("page");
 page_welcome.addEventListener("wheel", function (event) {
     var deltaY = event.deltaY;
     if (deltaY > 0) {
-        page_welcome.style.display = "none";
+        welcomeFunc();
+    }
+});
+function welcomeFunc(){
+    page_welcome.style.display = "none";
         page.style.display = "grid";
         page.classList.add('animate-up');
         background.classList.add('animate-opacity');
         background.classList.add("background-overlay")
-    }
-});
+};
